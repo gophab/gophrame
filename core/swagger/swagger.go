9046428e964acab253b51f627af80eb29cecba87
Wenjun Shen <wenjun.shen@gmail.com>
2024-05-21 19:22:57 +0800

    update: remove docs

--- core/swagger/swagger.go
+++ core/swagger/swagger.go
@@ -2,13 +2,12 @@ package swagger
 
 import (
 	"github.com/wjshen/gophrame/core/engine"
-	_ "github.com/wjshen/gophrame/docs"
 
-	ginSwagger "github.com/swaggo/gin-swagger"
+	swagger "github.com/swaggo/gin-swagger"
 	"github.com/swaggo/gin-swagger/swaggerFiles"
 )
 
 func init() {
 	// Swagger
-	engine.Get().GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler)) // API 注释
+	engine.Get().GET("/swagger/*any", swagger.WrapHandler(swaggerFiles.Handler)) // API 注释
 }
