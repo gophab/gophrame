30343f3aa58ec7c2de3ca86f4a4eaa8e5beee542
Wenjun Shen <wenjun.shen@gmail.com>
2024-05-23 20:14:18 +0800

    update

--- default/module.go
+++ default/module.go
@@ -1,5 +1,23 @@
 package module
 
+import (
+	"github.com/wjshen/gophrame/core/starter"
+
+	_ "github.com/wjshen/gophrame/default/controller"
+	_ "github.com/wjshen/gophrame/default/security"
+	_ "github.com/wjshen/gophrame/default/service"
+)
+
 const (
 	MODULE_ID = 1
 )
+
+func init() {
+	starter.RegisterStarter(Start)
+
+	// 1. 加载Config...
+}
+
+func Start() {
+
+}
