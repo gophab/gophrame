f941610fab989488045cc128e0dbba9a7a8deabd
申文俊 <wenjun.shen@gmail.com>
2024-08-03 23:01:01 +0800

    update

--- core/util/collection/set.go
+++ core/util/collection/set.go
@@ -15,13 +15,25 @@ func (s Set[T]) Add(key T) {
 	s[key] = struct{}{}
 }
 
+func (s Set[T]) AddAll(keys []T) {
+	for _, key := range keys {
+		s[key] = struct{}{}
+	}
+}
+
 func (s Set[T]) Delete(key T) {
 	delete(s, key)
 }
 
+func (s Set[T]) DeleteAll(keys []T) {
+	for _, key := range keys {
+		delete(s, key)
+	}
+}
+
 func (s Set[T]) AsList() []T {
 	var result = make([]T, 0)
-	for k, _ := range s {
+	for k := range s {
 		result = append(result, k)
 	}
 	return result
