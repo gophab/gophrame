f754eb044f4384ba91488bbc59863888f20beafd
Wenjun Shen <wenjun.shen@gmail.com>
2024-10-15 17:35:34 +0800

    update

--- module/common/domain/content.go
+++ module/common/domain/content.go
@@ -8,10 +8,10 @@ type ContentTemplateInfo struct {
 	domain.ParametersEnabled
 	domain.PropertiesEnabled
 	Name    string `gorm:"column:name" json:"name,omitempty"`
-	Title   string `gorm:"column:title" json:"title,omitempty"`
+	Title   string `gorm:"column:title" json:"title,omitempty" i18n:"true"`
 	Type    string `gorm:"column:type" json:"type,omitempty"`
 	Scene   string `gorm:"column:scene" json:"scene,omitempty"`
-	Content string `gorm:"column:content" json:"content,omitempty"`
+	Content string `gorm:"column:content" json:"content,omitempty" i18n:"true"`
 	Status  int    `gorm:"column:status" json:"status"`
 }
 
