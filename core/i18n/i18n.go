a69c9913da92f821bc2869ef5ee3f180a319e3a5
申文俊 <wenjun.shen@gmail.com>
2024-10-23 21:38:49 +0800

    update

--- core/i18n/i18n.go
+++ core/i18n/i18n.go
@@ -19,3 +19,11 @@ func GetCurrentLanguage() string {
 	}
 	return locale.(string)
 }
+
+func SetCurrentLanguage(locale string) {
+	if locale == "" {
+		context.RemoveContextValue("_LOCALE_")
+	} else {
+		context.SetContextValue("_LOCALE_", locale)
+	}
+}
