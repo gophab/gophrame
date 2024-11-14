5afeb1827ba4fa0608822dc3e4551967cb26b0c0
申文俊 <wenjun.shen@gmail.com>
2024-11-14 22:22:51 +0800

    update

--- core/util/string.go
+++ core/util/string.go
@@ -237,3 +237,37 @@ func GenerateRandomNumeric(length int) string {
 
 	return string(result)
 }
+
+// 定义script的正则表达式，去除js可以防止注入
+var scriptRegex = regexp.MustCompile(`<script[^>]*?>[\s\S]*?<\\/script>`)
+
+// 定义style的正则表达式，去除style样式，防止css代码过多时只截取到css样式代码
+var styleRegex = regexp.MustCompile(`<style[^>]*?>[\s\S]*?<\/style>`)
+
+// 定义HTML标签的正则表达式，去除标签，只提取文字内容
+var htmlRegex = regexp.MustCompile(`<[^>]+>`)
+
+// 定义空格,回车,换行符,制表符
+var spaceRegex = regexp.MustCompile(`\s*|\t|\r|\n`)
+
+func RemoveHTMLTags(htmlStr string) string {
+	//定义字符串
+	// htmlStr = "<script type>var i=1; alert(i)</script><style> .font1{font-size:12px}</style><span>少年中国说。</span>红日初升，其道大光。<h3>河出伏流，一泻汪洋。</h3>潜龙腾渊， 鳞爪&nbsp;&nbsp;飞扬。乳 虎啸  谷，百兽震惶。鹰隼试翼，风尘吸张。奇花初胎，矞矞皇皇。干将发硎，有作&nbsp其芒。天戴其苍，地履其黄。纵有千古，横有" +
+	// 					"八荒。<a href=\"www.baidu.com\">前途似海，来日方长</a>。<h1>美哉我少年中国，与天不老！</h1><p>壮哉我中国少年，与国无疆！</p>";
+	// 过滤script标签
+	htmlStr = scriptRegex.ReplaceAllString(htmlStr, "")
+	// 过滤style标签
+	htmlStr = styleRegex.ReplaceAllString(htmlStr, "")
+	// 过滤html标签
+	htmlStr = htmlRegex.ReplaceAllString(htmlStr, "")
+	// 过滤空格等
+	htmlStr = spaceRegex.ReplaceAllString(htmlStr, "")
+	// 过滤&nbsp;
+	htmlStr = strings.ReplaceAll(htmlStr, "&nbsp;", "")
+	// 过滤&nbsp
+	htmlStr = strings.ReplaceAll(htmlStr, "&nbsp", "")
+	// 返回文本字符串
+	htmlStr = strings.Trim(htmlStr, " \t\r\n")
+
+	return htmlStr
+}
