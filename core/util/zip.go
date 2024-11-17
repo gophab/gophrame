cc32b6b4d0829287b56e1ae980de52759b65e41f
申文俊 <wenjun.shen@gmail.com>
2024-11-17 18:57:57 +0800

    update

--- core/util/zip.go
+++ core/util/zip.go
@@ -52,6 +52,79 @@ func Unzip(zipFile string, destDir string) ([]string, error) {
 	return paths, nil
 }
 
+func isDir(path string) (bool, error) {
+	// 尝试获取文件信息
+	fileInfo, err := os.Stat(path)
+
+	if err != nil {
+		// 如果发生错误（例如文件不存在），返回错误
+		return false, err
+	}
+	// 使用IsDir方法检查是否是目录
+	return fileInfo.IsDir(), nil
+}
+
+func dirFiles(dir string) ([]string, error) {
+	var files []string
+	//方法一
+	var walkFunc = func(path string, info os.FileInfo, err error) error {
+		files = append(files, path)
+		return nil
+	}
+	err := filepath.Walk(dir, walkFunc)
+	return files, err
+}
+
+func zipDir(zipWriter *zip.Writer, file string, oldForm, newForm string) error {
+	files, err := dirFiles(file)
+	if err == nil {
+		for _, file := range files {
+			zipFile(zipWriter, file, oldForm, newForm)
+		}
+	}
+	return err
+}
+
+func zipFile(zipWriter *zip.Writer, file string, oldForm, newForm string) error {
+	if b, err := isDir(file); err != nil {
+		return err
+	} else if b {
+		return zipDir(zipWriter, file, oldForm, newForm)
+	}
+
+	zipFile, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer zipFile.Close()
+	// 获取file的基础信息
+	info, err := zipFile.Stat()
+	if err != nil {
+		return err
+	}
+
+	header, err := zip.FileInfoHeader(info)
+	if err != nil {
+		return err
+	}
+
+	// 使用上面的FileInforHeader() 就可以把文件保存的路径替换成我们自己想要的了，如下面
+	header.Name = strings.Replace(file, oldForm, newForm, -1)
+
+	// 优化压缩
+	// 更多参考see http://golang.org/pkg/archive/zip/#pkg-constants
+	header.Method = zip.Deflate
+
+	writer, err := zipWriter.CreateHeader(header)
+	if err != nil {
+		return err
+	}
+	if _, err = io.Copy(writer, zipFile); err != nil {
+		return err
+	}
+	return nil
+}
+
 func ZipFiles(filename string, files []string, oldForm, newForm string) error {
 	newZipFile, err := os.Create(filename)
 	if err != nil {
@@ -68,43 +141,7 @@ func ZipFiles(filename string, files []string, oldForm, newForm string) error {
 
 	// 把files添加到zip中
 	for _, file := range files {
-
-		err = func(file string) error {
-			zipFile, err := os.Open(file)
-			if err != nil {
-				return err
-			}
-			defer zipFile.Close()
-			// 获取file的基础信息
-			info, err := zipFile.Stat()
-			if err != nil {
-				return err
-			}
-
-			header, err := zip.FileInfoHeader(info)
-			if err != nil {
-				return err
-			}
-
-			// 使用上面的FileInforHeader() 就可以把文件保存的路径替换成我们自己想要的了，如下面
-			header.Name = strings.Replace(file, oldForm, newForm, -1)
-
-			// 优化压缩
-			// 更多参考see http://golang.org/pkg/archive/zip/#pkg-constants
-			header.Method = zip.Deflate
-
-			writer, err := zipWriter.CreateHeader(header)
-			if err != nil {
-				return err
-			}
-			if _, err = io.Copy(writer, zipFile); err != nil {
-				return err
-			}
-			return nil
-		}(file)
-		if err != nil {
-			return err
-		}
+		zipFile(zipWriter, file, oldForm, newForm)
 	}
 	return nil
 }
