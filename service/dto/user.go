4a0bc6889e077e02f0d36da910cd8e1d251beebf
Wenjun Shen <wenjun.shen@gmail.com>
2026-01-19 14:27:56 +0800

    update: 用户敏感信息

--- service/dto/user.go
+++ service/dto/user.go
@@ -2,15 +2,15 @@ package dto
 
 type User struct {
 	Id            *string `json:"id"`
-	Login         *string `json:"login"`
-	Mobile        *string `json:"mobile"`
-	Email         *string `json:"email"`
+	Login         *string `json:"login,omitempty"`
+	Mobile        *string `json:"mobile,omitempty"`
+	Email         *string `json:"email,omitempty"`
 	Password      *string `json:"-"`
 	PlainPassword *string `json:"password,omitempty"`
-	Name          *string `json:"name"`
-	InviteCode    *string `json:"inviteCode"`
-	InviterId     *string `json:"inviterId"`
-	SocialId      *string `json:"socialId"`
-	Admin         *bool   `json:"admin"`
-	TenantId      *string `json:"tenantId"`
+	Name          *string `json:"name,omitempty"`
+	InviteCode    *string `json:"inviteCode,omitempty"`
+	InviterId     *string `json:"inviterId,omitempty"`
+	SocialId      *string `json:"socialId,omitempty"`
+	Admin         *bool   `json:"admin,omitempty"`
+	TenantId      *string `json:"tenantId,omitempty"`
 }
